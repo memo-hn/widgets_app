e0761195b7b7dd213cd5647d601704d568327888
Memo-hn <virtualdmkt@gmail.com>
2024-01-21 00:45:42 -0600

    Menu con navigator

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter + Material3'),
+        title: const Text('Flutter + Material 3'),
           
       ),
       body: const _HomeView(),
@@ -48,11 +49,16 @@ class _CustomListTile extends StatelessWidget {
 
     return ListTile(
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
-      leading: Icon(menuItem.icon, color: colors.primary,),
+      leading: Icon(menuItem.icon, color: colors.primary, size: 40,),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () { 
-        //TODO: navegar a otra pantalla
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const ButtonsScreen(),
+        ),
+      );
        },
       
     );
